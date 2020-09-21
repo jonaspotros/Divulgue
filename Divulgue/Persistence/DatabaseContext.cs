067d4d7fb37ec751ddbd523ff6b388e183683195
Jonas Potros <jonaspotros@gmail.com>
2020-09-21 11:11:06 -0300

    gitIgnore

---
+++ Divulgue/Persistence/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Divulgue.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Divulgue.Persistence
+{
+    public class DatabaseContext: DbContext
+    {
+        public DbSet<Usuario> usuarios { get; set; }
+    }
+}
